8e440cef8cd89be6baa0d3e76d76be82feb336ac
ghecko <ghecko78@gmail.com>
2020-06-30 14:32:02 +0200

    change https URL to git URL

--- git_init.sh
+++ git_init.sh
@@ -1,3 +1,3 @@
-git remote set-url origin --push --add https://github.com/immunIT/owfmodules.uart.baudrates
+git remote set-url origin --push --add git@github.com:immunIT/owfmodules.uart.baudrates.git
 git remote set-url origin --push --add git@bitbucket.org:octowire/owfmodules.uart.baudrates.git
 git remote -v
